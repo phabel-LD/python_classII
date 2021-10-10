d8cc306203df43e761c4993ce4d13c7e82c27ec2
phabel <phabel2001@gmail.com>
2021-10-10 16:51:26 -0500

    Corrections. Upgrade Part II

--- Tasks/Entrez.py
+++ Tasks/Entrez.py
@@ -9,14 +9,16 @@
   Phabel Antonio Lopez Delgado <phabel2001@gmail.com>
   
 ## DATE:
-  October 02nd, 2021.
+  October 10th, 2021.
   
 ## DESCRIPTION & LOGIC:
   This script solves two tasks. The first one is to access
   two specific fiels and subfields from the database "protein"
   and print their description. The second one automatizes an
   esearch with a string of key terms, retrieves files' IDs and
-  writes a new file with them.
+  writes a new file with them. Then uses the id's in the file
+  to access the abstracts and citations from PubMed and write
+  a second file.
   
 ## USAGE:
   Entrez.py python 3.8.5
@@ -30,7 +32,9 @@
         
 
     OUTPUT: A file with all IDs
-        according to search_terms
+        according to search_terms.
+        A file with all abstracts
+        and citations per ID
 
 
 ## SOFTWARE REQUIREMENTS:
@@ -42,18 +46,24 @@
     
     
 ## LAST MODIFICATION:
-  Phabel Antonio Lopez Delgado: October 02nd, 2021  [Creation]
+  Phabel Antonio López Delgado: October 02nd, 2021  [Creation]
+  Phabel Antonio López Delgado: October 10th, 2021  [Upgrade to Part II]
 
 ## SOURCE:
   GitHub: https://github.com/phabel-LD/python_classII/blob/master/Tasks/Entrez.py
   
 '''
+# Check for permissions
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
 
 # Library
-from Bio import Entrez
+from Bio import Entrez, SeqIO
 
 
-# Main Code
+# Main Code ############
+
+# Part I ###############
 
 # Task 1: Print fileds' description
 # email reference
@@ -62,22 +72,52 @@ Entrez.email = "phabel@lcg.unam.mx"
 handle = Entrez.einfo(db = "protein")
 record = Entrez.read(handle)
 handle.close()
-print(record["DbInfo"]["FieldList"]["ECNO"])
-print(record["DbInfo"]["LinkList"]["protein_protein_small_genome"])
 
+# Print description for FieldList "ECNO"
+for field in record["DbInfo"]["FieldList"]:
+    if field["Name"]  == "ECNO":
+        print(field["Description"])
+
+# Print description for LinkList "protein_protein_small_genome"
+for field in record["DbInfo"]["LinkList"]:
+    if field["Name"]  == "protein_protein_small_genome":
+        print(field["Description"])
+        
 
 # Task 2: Use esearch to write IDs file
 # Create term
-search_terms = "(Amaranta Manrique[Author]) AND (alacranes[Title] OR etica[Title])"
+author = "Jie Su"
+title_word1 = "microRNA"
+title_word2 = "mice"
+search_terms = f"{author} AND ({title_word1} OR {title_word2})"
 # Access info with Entrez and esearch
-handle = Entrez.esearch(term = search_terms)
+count = int(record["DbInfo"]["Count"])
+handle = Entrez.esearch(db = "pubmed", term = search_terms, retmax = count)
 record = Entrez.read(handle)
 handle.close()
 IDs = record["IdList"]
-print(IDs)
+print(f"IDs({len(IDs)}): {IDs}")
 # Create file with all ID's
-file_name = "IDs.txt"
-with open(file_name, "w") as file:
+with open("IDs.txt", "w") as file_ids:
     for id_name in IDs:
-        file_name.write(id_name)
+        file_ids.write(f"{id_name}\n")
 
+# Part II ###############
+# Open abst_file in "w" mode, and IDs.txt just with "r+" mode
+with open("Abstracts.txt", "w") as abst_file:
+    with open("IDs.txt", "r+") as file_ids:
+        # Get the info (abstract and citations) for every id in the file
+        for id_name in file_ids:
+            # Use efetch to retrieve abstracts 
+            fetch_handle = Entrez.efetch(db="pubmed", id = id_name,
+                                rettype="abstract", retmode="text")
+            data = fetch_handle.read()
+            fetch_handle.close()
+            abst_file.write(data)
+            
+            # Get citations
+            citations_results = Entrez.read(Entrez.elink(dbfrom="pubmed", db="pmc",
+                                                 LinkName = "pubmed_pmc_refs", from_uid = id_name))
+            # Write every citations in abst_file
+            for citation in citations_results[0]["LinkSetDb"]:
+                abst_file.write(str(citations_results[0]["LinkSetDb"]))
